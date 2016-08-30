84f5ff62720d71f05a65f119ed09f2e847af8939
Valerie Gibson <vgibson14@gmail.com>
2016-08-30 18:54:50 +0400

    now with added clicks

--- client/src/app.js
+++ client/src/app.js
@@ -2,5 +2,15 @@ var ChordyRunner = require('./chordy/models/chordyRunner')
 window.onload = function () {
   var chordy = new ChordyRunner();
   var result = chordy.findChord(["X", "3", "2", "0", "1", "0"]);
-
+  console.log(result);
+  var elements = document.querySelectorAll(".js-fret > div > p");
+  
+  for(var i =0; i < elements.length; i++){
+      var elem = elements[i];   
+      elem.onclick = function(e){
+          alert(e.currentTarget.innerText);
+          return false;
+      };
+  }
+  
 }
