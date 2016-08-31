56eaa45649ec1e5f879f770e09c2f46fc21405b9
Valerie Gibson <vgibson14@gmail.com>
2016-08-31 16:43:50 +0400

    tidies up the rendering bit

--- client/src/app.js
+++ client/src/app.js
@@ -1,41 +1,12 @@
-var Handlebars = require('handlebars');
 var ChordyRunner = require('./chordy/models/chordyRunner')
+var Fretboard = require('./ui/fretboard')
 window.onload = function () {
-  createFretboard();
+  renderUI();
 
   var chordy = new ChordyRunner();
-  var result = chordy.findChord(["X", "3", "2", "0", "1", "0"]);
-  console.log(result);
-  var elements = document.querySelectorAll(".js-fret > div > p");
-  
-  for(var i =0; i < elements.length; i++){
-    var elem = elements[i];   
-    elem.onclick = function(e){
-      alert(e.currentTarget.innerText);
-      return false;
-    };
+  var result = chordy.findChord(["X", "3", "2", "0", "1", "0"]); 
+
+  function renderUI(){
+    new Fretboard();
   }
-
-  function createFretboard() {
-    var source = document.getElementById("fret-template").innerHTML;
-    var template = Handlebars.compile(source);
-    var neck = document.getElementById("neck");
-    var notes = [
-      {string:"1", value:"e"},
-      {string:"2", value:"b"},
-      {string:"3", value:"g"},
-      {string:"4", value:"d"},
-      {string:"5", value:"a"},
-      {string:"6", value:"e"},
-      {string:"spacer"}
-    ];
-
-    for(var i = 0; i < 22;i++){
-      var context = {fret_id:(i + 1), notes: notes}
-      var html = template(context);
-
-      neck.innerHTML = neck.innerHTML + html;
-    }
-  }
-  
 }
---
+++ client/src/ui/fretboard.js
@@ -0,0 +1,42 @@
+var Handlebars = require('handlebars');
+
+var Fretboard = function(){
+  this.notes = [
+    {string:"1", value:"e"},
+    {string:"2", value:"b"},
+    {string:"3", value:"g"},
+    {string:"4", value:"d"},
+    {string:"5", value:"a"},
+    {string:"6", value:"e"},
+    {string:"spacer"} //yuck, this it to add the first space to the left of each fret
+  ];
+  this.render();
+  this.attachEvents();
+}
+
+Fretboard.prototype = {
+  render: function() {
+    var source = document.getElementById("fret-template").innerHTML;
+    var template = Handlebars.compile(source);
+    var neck = document.getElementById("neck");
+
+    for(var i = 0; i < 22;i++){
+      var context = {fret_id:(i + 1), notes: this.notes}
+      var html = template(context);
+      neck.innerHTML = neck.innerHTML + html;
+    }
+  },
+  attachEvents: function(){
+    var elements = document.querySelectorAll(".js-fret > div > p");
+    
+    for(var i =0; i < elements.length; i++){
+      var elem = elements[i];   
+      elem.onclick = function(e){
+        alert(e.currentTarget.innerText);
+        return false;
+      };
+    } 
+  }
+}
+
+module.exports = Fretboard;
