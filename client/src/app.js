38e5af3419351b56efa59697757ba077cd579019
Valerie Gibson <vgibson14@gmail.com>
2016-09-02 13:12:06 +0400

    spaces

--- client/src/app.js
+++ client/src/app.js
@@ -3,7 +3,9 @@ var Fretboard = require('./ui/fretboard')
 var Notes = require('./ui/notes')
 var DomState = require('./ui/domState')
 var TemplateEngine = require('./ui/templateEngine')
+
 window.onload = function () {
+
   var chordy = new ChordyRunner();
   renderUI(chordy);
 
